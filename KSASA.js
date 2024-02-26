2900627ce183835cc68bbb2779444bd30cdfc513
yaf3iaden <42390257+noorromas@users.noreply.github.com>
2024-02-26 06:42:02 +0300

    Update KSASA.js
    ي

--- KSASA.js
+++ KSASA.js
@@ -42,7 +42,7 @@ border: 1px solid #efc33d;
 margin-bottom: 3px !important;
 margin-top: 2px !important;
 border-radius: 4px;
-background-image: url(https://up6.cc/2024/02/170890980140031.gif);
+background-image: url(https://up6.cc/2024/02/170891890732181.gif);
 background-size: cover;
 box-shadow: 0 0 0px rgb(255, 255, 255), inset 0 0 5px rgb(255, 255, 255), 0 0 0 0px #000;
 }
@@ -137,7 +137,7 @@ border: 1px solid #efc33d;
 margin-bottom: 3px !important;
 margin-top: 2px !important;
 border-radius: 4px;
-background-image: url(https://up6.cc/2024/02/17089162028981.gif);
+background-image: url(https://up6.cc/2024/02/170891799598311.png);
 background-size: cover;
 box-shadow: 0 0 0px rgb(255, 255, 255), inset 0 0 5px rgb(255, 255, 255), 0 0 0 0px #000;
 }
