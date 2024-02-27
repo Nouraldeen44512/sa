fc880d82511ea158e388b14d55fc063ded84123a
yaf3iaden <42390257+noorromas@users.noreply.github.com>
2024-02-27 03:44:18 +0300

    Update KSASA.js
    ي

--- KSASA.js
+++ KSASA.js
@@ -78,9 +78,9 @@ box-shadow: 0 0 0px rgb(255, 255, 255), inset 0 0 5px rgb(255, 255, 255), 0 0 0 
   cursor: pointer;
 }
 `);
-var me_user1=setInterval(function(){
+var me_user2=setInterval(function(){
     if(myid!=null){
-        clearInterval(me_user1);
+        clearInterval(me_user2);
         $("#users .uzr:contains('تنإهُيد')").addClass('tnahed1');
         $("#users .uzr.tnahed1 .c-flex.fr").append('<img class="My_star1" src="https://up6.cc/2024/02/170891465338531.gif">');
 	      $("#users .uzr span.u-topic:contains('تنإهُيد')").after('<img id="mystory1" class="My_vedio1" title="ستوري" src="https://i.ibb.co/47VS4nY/image.png">');
