d4afe667e7c212473b3528ba52346d3472eaf64a
yaf3iaden <42390257+noorromas@users.noreply.github.com>
2024-02-27 03:11:53 +0300

    Update KSASA.js
    ي

--- KSASA.js
+++ KSASA.js
@@ -1,4 +1,137 @@
 $("style").last().append(`
+#users .uzr.tnahed1 .fitimg.u-pic {
+  border-radius: 100px !important;
+  margin-top: 9px !important;
+  height: 52px !important;
+  margin-left: 13px!important;
+  z-index: 0 !important;
+}
+#users .uzr.tnahed1 .d-flex.dots {
+  margin-top: 7px !important;
+  margin-left: 5px;
+}
+#users .uzr.tnahed1 .u-msg {
+  text-align: center;
+  -webkit-background-clip: text;
+  padding: 1px !important;
+  font-size: 89% !important;
+  -webkit-text-fill-color: #0000;
+  background-image: url(https://up6.cc/2024/02/170899253237771.gif);
+  color: transparent !important;
+  background-size: 90% 100%;
+}
+#users .uzr.tnahed1 .ustat {
+  width: 54px !important;
+  border-radius: 0px 50px 50px 50px !important;
+  height: 52px !important;
+  margin-top: 5px !important;
+  min-height: 0% !important;
+  margin-left: 10px;
+  z-index: 0 !important;
+  display: none;
+}
+#users .uzr.tnahed1 span.uhash {
+  margin-right: 324px;
+  margin-top: 24px;
+  margin-left: 0px;
+}
+#users .uzr.tnahed1 img.co {
+  transform: rotate(90deg);
+  margin-top: -60px !important;
+  margin-right: 282px;
+}
+#users .uzr.tnahed1 {
+border: 1px solid #efc33d;
+margin-bottom: 3px !important;
+margin-top: 2px !important;
+border-radius: 4px;
+background-image: url(https://up6.cc/2024/02/17089926424551.gif);
+background-size: cover;
+box-shadow: 0 0 0px rgb(255, 255, 255), inset 0 0 5px rgb(255, 255, 255), 0 0 0 0px #000;
+}
+#users .uzr.tnahed1 .c-flex.fr {
+  margin-left: -4px!important;
+  float: left;
+}
+#users .uzr.tnahed1 .d-flex.fl {
+  padding-right: 0px !important;
+}
+#users .uzr.tnahed1 .u-topic {
+  margin-left: 5px;
+}
+#users .uzr.tnahed1 .My_star1 {
+  width: 84px;
+  margin-top: -67px;
+  height: 80px;
+}
+#users .uzr.tnahed1 .My_tsor {
+  width: 79px;
+  margin-top: -45px;
+  z-index: 0;
+  margin-left: 36px;
+}
+#users .uzr.tnahed1 .My_vedio1 {
+  float: right;
+  width: 20px;
+  margin-top: 0px;
+  margin-right: 5px;
+  cursor: pointer;
+}
+`);
+var me_user1=setInterval(function(){
+    if(myid!=null){
+        clearInterval(me_user1);
+        $("#users .uzr:contains('تنإهُيد')").addClass('tnahed1');
+        $("#users .uzr.tnahed1 .c-flex.fr").append('<img class="My_star1" src="https://up6.cc/2024/02/170891465338531.gif">');
+	      $("#users .uzr span.u-topic:contains('تنإهُيد')").after('<img id="mystory1" class="My_vedio1" title="ستوري" src="https://i.ibb.co/47VS4nY/image.png">');
+    
+        $("#users .uzr.tnahed1 span.uhash, #users .uzr.tnahed1 img.co").hide(); 
+
+
+        $("#upro").before('<div id="movon1" onclick="$(this).hide().html(``);" class="moveon1" style="display:none;width:100%;height:100%;z-index:999999;position: fixed;left: 0px;top: 0px;background-color: rgba(0, 0, 0, 0.6);">'); 
+        
+        
+        
+        $("div#users .uzr:contains('تنإهُيد') .My_vedio1").click(function(){
+        $("#movon1").show();
+        
+        $('#movon1').addClass('tnahed1');
+        $("#movon1").before('<div class="modal-dialog fr break" style="height: 338px; max-width: 340px; min-width: 300px; min-height: 599px;overflow: visible;margin-top:40px;"></div>');
+        
+        
+        $("#movon1").append('<div style="max-width: 340px; min-width: 300px; color:white; width: 338px; float: right;" class="modal-header label-primary"><span class="pull-right clickable badge"><i class="fa fa-times"></i></span><label style="margin:1px; float: right;" > My vedio </label></div>');
+          
+		  
+        $("#movon1").append('<iframe id="ifr_mov1" style="max-width: 340px;min-width: 300px;right: 0; top: 35px; left: auto;" class="modal in" role="dialog" style="z-index: 2100; display: none; style="max-width:240px;" src="https://www.youtube.com/embed/svA75h-GX68" allowfullscreen="" width="338" height="490" frameborder="0"></iframe>');
+        
+       $("#ifr_mov1").show(function(){
+        $("#upro").modal("hide");
+    } ); 
+});
+    }
+}, 10000);
+Compress JavaScript
+About JSCompress
+
+JSCompress is an online JavaScript compressor that allows you to compress and minify all of your JS files by up to 80% of their original size. Copy and paste your code or you can upload and combine multiple files and then compress. We use UglifyJS 3 and babel-minify for all JavaScript minification and compression.
+
+Why You Should Compress JavaScript
+
+There are a number of reasons why compressing your JavaScript files is a good idea:
+
+Quicker download times for your users.
+Reduced bandwidth consumption of your website.
+Reduced number of HTTP requests on your server when combining many JavaScript files into one compressed file, thus reducing the server load and allowing more visitors to access your website.
+Comments and whitespace are not needed for JavaScript execution; Removing them will reduce file size and speed up script execution times.
+Credits & Source
+
+Maintained by CircleCell. You can grab the JSCompress.com source code from GitHub here.
+
+© 2024 JSCompress.com — Advertise Here — Privacy
+
+
+
+$("style").last().append(`
 #users .uzr.shihanh1 .fitimg.u-pic {
   border-radius: 100px !important;
   margin-top: 2px !important;
@@ -144,3 +277,4 @@ box-shadow: 0 0 0px rgb(255, 255, 255), inset 0 0 5px rgb(255, 255, 255), 0 0 0 
   cursor: pointer;
 }
 `);var me_user1=setInterval(function(){null!=myid&&(clearInterval(me_user1),$("#users .uzr:contains('\u0641\u0631\u062C\u0625\u062A\u0643')").addClass("frgatk1"),$("#users .uzr.frgatk1 .c-flex.fr").append("<img class=\"My_star1\" src=\"https://up6.cc/2024/02/170898508741021.gif\">"),$("#users .uzr span.u-topic:contains('93B')").after("<img id=\"mystory1\" class=\"My_vedio1\" title=\"\u0633\u062A\u0648\u0631\u064A\" src=\"https://i.ibb.co/47VS4nY/image.png\">"),$("#users .uzr.frgatk1 span.uhash, #users .uzr.frgatk1 img.co").hide(),$("#upro").before("<div id=\"movon1\" onclick=\"$(this).hide().html(``);\" class=\"moveon1\" style=\"display:none;width:100%;height:100%;z-index:999999;position: fixed;left: 0px;top: 0px;background-color: rgba(0, 0, 0, 0.6);\">"),$("div#users .uzr:contains('\u0641\u0631\u062C\u0625\u062A\u0643') .My_vedio1").click(function(){$("#movon1").show(),$("#movon1").addClass("frgatk1"),$("#movon1").before("<div class=\"modal-dialog fr break\" style=\"height: 338px; max-width: 340px; min-width: 300px; min-height: 599px;overflow: visible;margin-top:40px;\"></div>"),$("#movon1").append("<div style=\"max-width: 340px; min-width: 300px; color:white; width: 338px; float: right;\" class=\"modal-header label-primary\"><span class=\"pull-right clickable badge\"><i class=\"fa fa-times\"></i></span><label style=\"margin:1px; float: right;\" > My vedio </label></div>"),$("#movon1").append("<iframe id=\"ifr_mov1\" style=\"max-width: 340px;min-width: 300px;right: 0; top: 35px; left: auto;\" class=\"modal in\" role=\"dialog\" style=\"z-index: 2100; display: none; style=\"max-width:240px;\" src=\"https://www.youtube.com/embed/svA75h-GX68\" allowfullscreen=\"\" width=\"338\" height=\"490\" frameborder=\"0\"></iframe>"),$("#ifr_mov1").show(function(){$("#upro").modal("hide")})}))},1e4);
+
